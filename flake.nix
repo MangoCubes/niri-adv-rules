8c7a14fcf066ba81927013b5f28675cdfec04902
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2025-10-05 17:00:30 +0100

    Fix hash and add home manager

--- flake.nix
+++ flake.nix
@@ -12,54 +12,48 @@
       nixpkgs,
       flake-utils,
     }:
-    let
-      niri-adv-rules = flake-utils.lib.eachDefaultSystem (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          packages.default = pkgs.rustPlatform.buildRustPackage {
-            src = ./.;
-            name = "niri-adv-rules";
-            cargoHash = "sha256-m9TleJzwHpPYKrDOekGy4wfcvSbxEoO+pr2Yk3cBmos=";
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          src = ./.;
+          name = "niri-adv-rules";
+          cargoHash = "sha256-oXHOPbjFeIDQFhESmiy4wWF/4KNOTP4F1B3JYv1Wxw0=";
+        };
+        devShells.default = pkgs.mkShell {
+          env = {
+            RUST_BACKTRACE = "full";
           };
-          devShells.default = pkgs.mkShell {
-            env = {
-              RUST_BACKTRACE = "full";
-            };
-            packages = (
-              with pkgs;
-              [
-                rust-analyzer
-                lldb
-                jq
-                rustup
-                # This is necessary for opening bash from Neovim
-                bash
-              ]
-            );
-            shellHook =
-              let
-                initFile = pkgs.writeText ".bashrc" ''
-                  echo "Rust shell activated!"
-                  set -a
-                    hw() { echo "Hello world!"; }
-                    build() { nix build; }
-                    run() { build; ./result/bin/niri-adv-rules; }
-                  set +a
-                  # nvim .
-                '';
-              in
-              ''
-                bash --init-file ${initFile}; exit
+          packages = (
+            with pkgs;
+            [
+              rust-analyzer
+              lldb
+              jq
+              rustup
+              # This is necessary for opening bash from Neovim
+              bash
+            ]
+          );
+          shellHook =
+            let
+              initFile = pkgs.writeText ".bashrc" ''
+                echo "Rust shell activated!"
+                set -a
+                  hw() { echo "Hello world!"; }
+                  build() { nix build; }
+                  run() { build; ./result/bin/niri-adv-rules; }
+                set +a
+                # nvim .
               '';
-          };
-        }
-      );
-    in
-    {
-      default = niri-adv-rules.packages."${builtins.currentSystem}".default;
-      devShells = niri-adv-rules.devShells;
-    };
+            in
+            ''
+              bash --init-file ${initFile}; exit
+            '';
+        };
+      }
+    );
 }
